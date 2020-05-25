c602176c39dfb163de407f269337e8e4c4a55361
Dimitri Bilenkin <dbilenkin@gmail.com>
2020-05-24 22:35:45 -0400

    fixed status

--- solver.js
+++ solver.js
@@ -1,13 +1,14 @@
-function solve() {
-  const mustLetters = document.getElementById("mustLetters").value.toUpperCase();
-  const otherLetters = document.getElementById("otherLetters").value.toUpperCase();
-  const minLength = +document.getElementById("minLength").value || mustLetters.length;
+let mustLetters, otherLetters, minLength, solution, solutions;
 
-  const solution$ = document.getElementById("solution");
+function updateStatus(status) {
+  setTimeout(() => {
+    document.getElementById("status").innerHTML = status.toPrecision(4) + "%";
+  }, 0);
+}
 
-
-  let solutions = [];
-  for (let i = 0; i < words.length; i++) {
+function solve1000(i) {
+  let iters = i + 1000;
+  for (; i < iters && i < words.length; i++) {
     if (words[i].length < minLength) continue;
     let mustMatched = 0;
     let otherMatched = 0;
@@ -18,7 +19,8 @@ function solve() {
       let index = wordLetters.indexOf(letter);
       if (index !== -1) {
         mustMatched++;
-        wordLetters = wordLetters.slice(0, index) + wordLetters.slice(index + 1);
+        wordLetters =
+          wordLetters.slice(0, index) + wordLetters.slice(index + 1);
       }
     }
 
@@ -28,18 +30,48 @@ function solve() {
       let index = wordLetters.indexOf(letter);
       if (index !== -1) {
         otherMatched++;
-        wordLetters = wordLetters.slice(0, index) + wordLetters.slice(index + 1);
+        wordLetters =
+          wordLetters.slice(0, index) + wordLetters.slice(index + 1);
+      } else {
+        // break;
       }
     }
-    if ((mustMatched + otherMatched) === words[i].length) {
-      solutions.push({ word: words[i], score: mustMatched * 10 + words[i].length });
+    if (mustMatched + otherMatched === words[i].length) {
+      solutions.push({
+        word: words[i],
+        score: mustMatched * 10 + words[i].length
+      });
     }
   }
+  if (i === words.length) {
+    showSolutions();
+  }
+}
 
-  solutions.sort((w1, w2) => w2.score - w1.score).forEach(word => {
-    solution$.innerHTML += `
-    <div>${word.word}</div>
+function solve() {
+  mustLetters = document.getElementById("mustLetters").value.toUpperCase();
+  otherLetters = document.getElementById("otherLetters").value.toUpperCase();
+  minLength = +document.getElementById("minLength").value || mustLetters.length;
+
+  solution = document.getElementById("solution");
+  solution.innerHTML = "";
+
+  solutions = [];
+  for (let i = 0; i < words.length; i += 1000) {
+    updateStatus((100 * i) / words.length);
+    setTimeout(() => {
+      solve1000(i);
+    }, 0);
+  }
+}
+
+function showSolutions() {
+  solutions
+    .sort((w1, w2) => w2.score - w1.score);
+
+  for (let i = 0; i < 10; i++) {
+    solution.innerHTML += `
+    <div>${solutions[i].word}: ${solutions[i].score}</div>
     `;
-  })
-  
+  }
 }
